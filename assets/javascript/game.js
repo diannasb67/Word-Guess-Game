d21697104afc87e4a8d665972539ab78c1c6dae5
dsb67 <diannasb67@gmail.com>
2018-11-11 21:27:32 -0800

    second attempt uploading

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,3 @@
-
-// 2. Take the player’s guess.
-// 4. Check that the player’s guess is a valid letter.
-// 5. Keep track of letters the player has guessed.
-// 6. Show the player their progress.
-// 7. Finish when the player has guessed the word.
-
 // <!-- // Create an array of words for user to guess.  My theme is Travel -->
 
 var destinations = ["seychelles", "caribbean", "hawaii", "australia", "thailand", "fiji", "tahiti", "dubai"]
@@ -12,39 +5,31 @@ var destinations = ["seychelles", "caribbean", "hawaii", "australia", "thailand"
 /* // select words from array for user to guess and assign value to each word  */
 //ask TA about randomNum.
 
-
 var randomWord = destinations[Math.floor(Math.random()*destinations.length)];
-var correctLetter = [];
-var wrongLetter = [];
-var underScore = [];
-
-
 console.log(randomWord);
-document.getElementById("word").innerHTML = word [randomWord];
+$("#word").append(randomWord)
+
 
 // // create underscores based on word length 
-
-var underScore = [];
-
-var generateUnderscore = () => { 
-    for (var i = 0; i < word.length; i++) {
-        underScore.push('_');
-        }
-        return underScore;
-        }
-
-console.log(generateUnderscore());
-
+var answerArray = [];
+for (var i = 0; i < destinations.length; i++) {
+    answerArray[i] = "_";
+}
 
 // record the users key pressed using keyup
 
 document.addEventListener('keypress', (event) => { 
     var keycode = event.keyCode;
     var keyword = String.fromCharCode(event.keycode);
-    console.log(keyword);
-});
+    console.log(keycode);
+}); 
 
 // create rule that checks users guess
+var rightGuess = [];
+var wrongGuess = [];
+var usersScore = 0;
+
+var playerScore = document.getElementById("wins");
 
 
 
